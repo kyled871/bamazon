52729453f27c9fa6046319b4a8ba85fb39d06316
Kyle Davis <kyled871@yahoo.com>
2020-06-19 15:56:25 -0400

    connection function created

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,22 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql2');
+const { start } = require('repl');
+
+let connection = mysql.createConnection({
+
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "rootroot",
+    database: "bamazon_db",
+
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+
+    console.log(`Connected as id ${connection.threadId}. \n`);
+    start();
+
+});
 
