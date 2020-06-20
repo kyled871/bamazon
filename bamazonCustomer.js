ec8dbdf79cf498e835287fd0625218f6225d22ff
Kyle Davis <kyled871@yahoo.com>
2020-06-19 20:35:58 -0400

    added function to show all items currently in db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,13 +15,38 @@ connection.connect(function(err) {
     if (err) throw err;
 
     console.log(`Connected as id ${connection.threadId}. \n`);
-    start();
+    showItems()
+    // start();
 
 });
 
+function showItems() {
+    connection.query("SELECT * FROM products", function(err, response) {
+        if (err) throw err;
 
-function start() {
 
-    console.log('gh test.')
+        let allItems = () => {
+            console.log("*** Welcome to Bamazon! ***")
+            response.forEach(items => {
+                console.log(`ID: ${items.item_id}\nItem: ${items.product_name}\nDept Name: ${items.department_name}\nPrice: ${items.price}\n\n`)
+            });
+        }
+        allItems()
+
+        connection.end();
+      });
 
 }
+
+
+// function start() {
+
+//     inquirer.prompt([
+//         {
+
+
+
+//         }
+//     ])
+
+// }
