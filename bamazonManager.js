b0de3dc170a91cb5a24240ba6f3ae75133df052d
Kyle Davis <kyled871@yahoo.com>
2020-06-23 17:57:54 -0400

    added low inv function/ currently shows lower than 20

--- bamazonManager.js
+++ bamazonManager.js
@@ -16,53 +16,90 @@ connection.connect(function(err) {
 
     console.log(`Connected as id ${connection.threadId}. \n`);
     startManagerView();
-    
 
 });
 
+function optionChoices() {
+
+    inquirer.prompt([
+        {
+            name: 'options',
+            type: 'list',
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+            message: 'Please choose an option:'
+        }
+        
+    ]).then(function(answers) {
+
+        let managersAnswer = answers.options
+
+        switch (managersAnswer) {
+            case 'View Products for Sale':
+                viewProducts();
+                break;
+
+            case 'View Low Inventory':
+                lowInventory();
+                break;
+
+            case 'Add to Inventory':
+                addInventory();
+                break;
+
+            case 'Add New Product':
+                addProduct();
+                break;
+        
+            default:
+                break;
+        }
+
+    })
+    
+}
+
 function startManagerView() {
 
-    function optionChoices() {
+    console.log(`\nWelcome Bamazon Manager!\n`)
 
-        console.log(`\nWelcome Bamazon Manager!\n`)
-        inquirer.prompt([
-            {
-                name: 'options',
-                type: 'list',
-                choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
-                message: 'Please choose an option:'
-            }
-            
-        ]).then(function(answers) {
-
-            let managersAnswer = answers.options
-
-            switch (managersAnswer) {
-                case 'View Products for Sale':
-                console.log('here are the products')
-                    break;
-
-                case 'View Low Inventory':
-                    console.log('here is the low inventory')
-                    break;
-
-                case 'Add to Inventory':
-                    console.log('added to inventory')
-                    break;
-
-                case 'Add New Product':
-                    console.log('add to products!')
-                    break;
-            
-                default:
-                    break;
-            }
-
-        })
-        
-    }
     optionChoices()
+};
 
 
+// functions for each choice --------------------------------
+function viewProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
 
-}
+        console.log(`\nHere are the current products for sale: \n\n`);
+        res.forEach(item => {
+            console.log(`ID: ${item.item_id}\nItem: ${item.product_name}\nPrice: ${item.price}\nQty: ${item.stock_quantity}\n\n`);      
+        });
+        optionChoices();
+    });
+
+};
+
+function lowInventory() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        console.log(`\nThese products need restocking: \n\n`);
+        res.forEach(item => {
+            if (item.stock_quantity < 20) {
+                console.log(`ID: ${item.item_id}\nItem: ${item.product_name}\nPrice: ${item.price}\nQty: >>> ${item.stock_quantity} <<< --- LOW!\n\n`);
+            }
+        });
+
+        optionChoices();
+    });
+
+};
+
+function addInventory() {
+
+};
+
+function addProduct() {
+
+};
